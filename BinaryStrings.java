fc4f3eed30652f61177601c783d2b5b571ea6ee2
manasamj64-dotcom <manasamj64@gmail.com>
2026-03-20 16:27:00 +0530

    Add files via upload
    module 9 java program

---
+++ BinaryStrings.java
@@ -0,0 +1,18 @@
+package assignments.module9;
+
+public class BinaryStrings {
+        static void generate(String str, int n) {
+            if (str.length() == n) {
+                System.out.println(str);
+                return;
+            }
+
+            generate(str + "0", n);
+            generate(str + "1", n);
+        }
+
+        public static void main(String[] args) {
+            generate("", 3);
+        }
+    }
+
